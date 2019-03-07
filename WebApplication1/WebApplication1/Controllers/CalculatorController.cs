155751049530b47d7d3b320074e40605ca72e273
Nguyen Anh Tu <tu.lqd.163@gmail.com>
2019-03-07 15:07:42 +0700

    Template

---
+++ WebApplication1/WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        //
+        // GET: /Calculator/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string ShowAuthor()
+        {
+            return "Nguyen Anh Tu";
+        }
+        public double Factorial(int id)
+        {
+            int n = id;
+            double fac = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fac = fac * i;
+            }
+            return fac;
+        }
+        public int Sum(int a, int b)
+        {
+            int s = a + b;
+            return s;
+        }
+	}
+}
