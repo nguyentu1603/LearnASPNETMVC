d11e392a938c2bd7ccbcc335acf4888e3490a839
Nguyen Anh Tu <tu.lqd.163@gmail.com>
2019-03-07 15:30:46 +0700

    Update

---
+++ WebApplication1/UnitTestProject1/CalculatorControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System.Web.Mvc;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CalculatorControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            CalculatorController controller = new CalculatorController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+         [TestMethod]
+        public void TestAuthor()
+        {
+            var controller = new CalculatorController();
+            var result = controller.ShowAuthor();
+            Assert.AreEqual("Nguyen Anh Tu", result);
+        }
+    }
+}
